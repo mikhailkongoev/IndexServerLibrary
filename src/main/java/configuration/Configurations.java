c055842f1107637aeb669709d46fd4f915fc8862
mikhailkongoev <mikhailkongoev@mail.ru>
2019-07-20 22:44:06 +0300

    entity manager using added

--- src/main/java/configuration/Configurations.java
+++ src/main/java/configuration/Configurations.java
@@ -3,9 +3,13 @@ package configuration;
 import interfaces.lexer.Lexer;
 import interfaces.services.ConfigurationService;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.util.concurrent.locks.ReadWriteLock;
 
 public class Configurations {
+    public static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("org.jetbrains.data.jpa.hibernate");
+
     public static ConfigurationService createConfiguration() {
         return new ConfigurationServiceImpl();
     }
--- src/main/java/index/AbstractIndexService.java
+++ src/main/java/index/AbstractIndexService.java
@@ -3,8 +3,14 @@ package index;
 import interfaces.lexer.Lexer;
 import interfaces.services.IndexService;
 
+import javax.persistence.EntityManager;
+
+import static configuration.Configurations.entityManagerFactory;
+
 public abstract class AbstractIndexService implements IndexService {
     protected Lexer lexer;
+
+    protected EntityManager em = entityManagerFactory.createEntityManager();
 
     AbstractIndexService(Lexer lexer) {
         this.lexer = lexer;
