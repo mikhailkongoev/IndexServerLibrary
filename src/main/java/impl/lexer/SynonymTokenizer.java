0dd8ccc9a90369b318b7f3e955cb635d42b88cb4
mikhailkongoev <mikhailkongoev@mail.ru>
2019-07-21 18:30:11 +0300

    Synonym tokenizer

---
+++ src/main/java/impl/lexer/SynonymTokenizer.java
@@ -0,0 +1,12 @@
+package impl.lexer;
+
+import interfaces.lexer.Lexer;
+
+import java.util.Collection;
+
+public class SynonymTokenizer implements Lexer {
+    @Override
+    public Collection<String> parse(String query) {
+        return null;
+    }
+}
