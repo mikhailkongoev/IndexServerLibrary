7f78d74d7b23a8ae50f21b77d64fb2496680a5f7
mikhailkongoev <mikhailkongoev@mail.ru>
2019-07-24 13:57:03 +0300

    added tokenizer implementations.

--- src/main/java/impl/lexer/StemmingTokenizer.java
+++ src/main/java/impl/lexer/StemmingTokenizer.java
@@ -2,11 +2,18 @@ package impl.lexer;
 
 import interfaces.lexer.Lexer;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class StemmingTokenizer implements Lexer {
     @Override
     public Collection<String> parse(String query) {
-        return null;
+        return Arrays.stream(query.trim().split("\\s+")).map(this::stemStr).collect(Collectors.toList());
+    }
+
+    private String stemStr(String keyword) {
+        //TODO: implement
+        return keyword;
     }
 }
--- src/main/java/impl/lexer/SynonymTokenizer.java
+++ src/main/java/impl/lexer/SynonymTokenizer.java
@@ -2,11 +2,19 @@ package impl.lexer;
 
 import interfaces.lexer.Lexer;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 public class SynonymTokenizer implements Lexer {
     @Override
     public Collection<String> parse(String query) {
-        return null;
+        return Arrays.stream(query.trim().split("\\s+")).flatMap(k -> getSynonyms(k).stream()).collect(Collectors.toList());
+    }
+
+    private Collection<String> getSynonyms(String keyword) {
+        //TODO: implement
+        return Collections.singletonList(keyword);
     }
 }
